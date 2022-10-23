50ae70ecc348a2163e8cec2d383cb40d391adfed
David Disu <daviddisu8@gmail.com>
2022-10-23 01:31:53 +0100

    Add modal notification

--- apps/backend-api/src/main.ts
+++ apps/backend-api/src/main.ts
@@ -1,18 +1,59 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+// importing libraries and dependencies
+import { Request, Response } from 'express'
+import express = require('express')
+import {createConnection} from 'typeorm'
+import bodyParser = require('body-parser')
+// import * as UserEndpoint from './api/user'
+import * as  dotenv from 'dotenv';
+dotenv.config();
 
-import * as express from 'express';
+export default (async () => {
+    try {
+        // establish database connection.
+        if (process.env.NODE_ENV === 'development') {
+            await createConnection();
+        } else {
+            await createConnection({
+                type: 'mongodb',
+                useNewUrlParser: true,
+                // tslint:disable-next-line:max-line-length
+                url: `mongodb+srv://${process.env.MONGODB_ATLAS_USERNAME}:${process.env.MONGODB_ATLAS_PASSWORD}@cluster0-uxdic.mongodb.net/test?retryWrites=true&w=majority`,
+                ssl: true,
+                entities: ['build/model/**/*.js'],
+            });
+        }
+        // tslint:disable-next-line:no-console
+        console.log('Database Connection Established...');
 
-const app = express();
+        // create a new express application instance
+        const app: express.Application = express();
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to backend-api!' });
-});
+        // the port the express app will listen on
+        const port: any = process.env.PORT || 2002;
 
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: false }));
+
+        // app.use('/user', UserEndpoint.router)
+
+        app.get('/', (req: Request, res: Response) => {
+            res.setHeader('Content-Type', 'application/json')
+            res.send(JSON.stringify({
+                status: 200,
+                message: 'Service is up!',
+            }))
+        })
+
+        // serve the application at the given port
+        app.listen(port, () => {
+            // tslint:disable-next-line:no-console
+            console.log(`Listening at http://localhost:${port}/`)
+        })
+
+        return app
+    } catch (error) {
+        // tslint:disable-next-line:no-console
+        console.error(`Error: ${error}`)
+        throw error
+    }
+})()
