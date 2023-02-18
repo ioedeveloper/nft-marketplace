8b65c92407e0290d3f39ad16d6b509481e741f64
ioedeveloper <daviddisu8@gmail.com>
2023-02-18 16:42:31 +0100

    Improve buy function

--- contracts/marketplace.sol
+++ contracts/marketplace.sol
@@ -1,51 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "./token.sol";
+import "hardhat/console.sol";
 
-contract MyToken is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
-    using CountersUpgradeable for CountersUpgradeable.Counter;
-
-    CountersUpgradeable.Counter private _tokenIdCounter;
-
-    struct Token {
-        address owner;
-        string ipfsHash;
-        string name;
-        uint16 price;
-        string description;
-        string contactAddress;
-        bool verified;
-    }
-
-    mapping(uint256 => Token) public tokensData;
+contract Marketplace is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+    MyTokenV2 public token;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
-    function initialize() initializer public {
-        __ERC721_init("NFT-Marketplace", "NFTM");
+    function initialize(MyTokenV2 _token) initializer public {
         __Ownable_init();
         __UUPSUpgradeable_init();
-    }
-
-    function safeMint(address to, string memory hash, string memory name, uint16 price, string memory contactAddress, string memory description) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        tokensData[tokenId].owner = to;
-        tokensData[tokenId].ipfsHash = hash;
-        tokensData[tokenId].name = name;
-        tokensData[tokenId].price = price;
-        tokensData[tokenId].contactAddress = contactAddress;
-        tokensData[tokenId].description = description;
-        tokensData[tokenId].verified = true;
+        token = _token;
     }
 
     function _authorizeUpgrade(address newImplementation)
@@ -54,20 +27,20 @@ contract MyToken is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSUp
         override
     {}
 
-    function publicMint (address to, string memory hash, string memory name, uint16 price, string memory contactAddress, string memory description) public {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        tokensData[tokenId].owner = to;
-        tokensData[tokenId].ipfsHash = hash;
-        tokensData[tokenId].name = name;
-        tokensData[tokenId].price = price;
-        tokensData[tokenId].contactAddress = contactAddress;
-        tokensData[tokenId].description = description;
-        tokensData[tokenId].verified = false;
+    function buy (address _from, address payable _to, uint256 _tokenId) public payable {
+        console.log("msg.value: ", msg.value);
+        uint256 commission = (msg.value * 10) / 100;
+        uint256 _tokenPrice = token.getTokenPrice(_tokenId);
+        console.log("tokenPrice: ", _tokenPrice);
+
+        require(msg.value >= _tokenPrice, "Insufficient funds to purchase NFT");
+        (bool sent, ) = _to.call{value:msg.value}("");
+        require(sent, "Ether transfer failed");
+        transferTokenOwnership(_from, _to, _tokenId);
     }
 
-    function verifyMint (uint256 tokenId) public onlyOwner {
-        tokensData[tokenId].verified = true;
+    function transferTokenOwnership (address _from, address _to, uint256 _tokenId) public {
+        token.transferFrom(_from, _to, _tokenId);
+        token.transferOwnershipTokensData(_to, _tokenId);
     }
 }
